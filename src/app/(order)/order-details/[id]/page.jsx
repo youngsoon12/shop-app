2410a1a455bbbdffa30ac2320f9e28178f82cd3e
youngsoon12 <enejwk789@gmail.com>
2023-11-10 18:22:36 +0900

    update : 2023-11-10

--- src/app/(order)/order-details/[id]/page.jsx
+++ src/app/(order)/order-details/[id]/page.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const OrderDetails = ({params, searchParams}) => {
   const {hello} = searchParams;
   const {id} = params;
+  console.log(hello);
   return (
     <div>
       {id} : {hello}
